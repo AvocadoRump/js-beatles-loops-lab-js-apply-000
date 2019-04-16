8869cf310ebb451efb0bbb0eaf3fa559edd07a76
punctual-skill-3845 <bichsonnguyen@gmail.com>
2019-04-16 00:09:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// add solution here
+
+function theBeatlesPlay(arr1, arr2) {
+  let returnArr = [];
+  for (let i=0;i<arr1.length;i++) {
+    returnArr.push(arr1[i] + ' ' + arr2[i]);
+  }
+  return returnArr;
+}
